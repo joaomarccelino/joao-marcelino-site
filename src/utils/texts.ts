12e448545329070ce72770a84a58baff0cd7645d
joaomarccelino <joao.marcelino.esc@gmail.com>
2021-07-04 16:28:03 -0300

    feat: tab icon

--- src/utils/texts.ts
+++ src/utils/texts.ts
@@ -1,15 +1,15 @@
 const ptBr = {
     about: 'quem sou eu?',
-    p1: 'Meu nome é João Lucas, sou músico, funcionário da área da educação e sempre inquieto, há alguns anos após ingressar na faculdade de Análise e Desenvolvimento de Sistemas fui descobrindo o meu gosto pelo desenvolvimento.',
+    p1: 'Meu nome é João Lucas, sou músico, secretário de escola e sempre inquieto. Há dois anos ingressei na faculdade de Análise e Desenvolvimento de Sistemas e foi lá que descobri o meu gosto pela programação.',
     p2: 'Comecei desenvolvendo em Wordpress e logo descobri o mundo do React e React Native que desde então venho estudando e aprendendo a cada dia.',
-    p3: 'Creio que chegou a hora de entrar de vez no mundo da programação e por isso acabei criando esse site para servir de plataforma para expor minhas ideias.',
+    p3: 'Creio que chegou a hora de entrar de vez no mundo da programação e por isso acabei criando esse site para servir de plataforma para expor meus projetos e ideias.',
 }
 
 const en = {
     about: 'who am i?',
-    p1: 'My name is João Lucas, musician, employee in the education area and always restless, a few years ago after joining the College of Systems Analysis and Development I discovered my taste for development.',
+    p1: "My name is João Lucas, I'm a musician, school secretary and always restless. Two years ago I joined the Systems Analysis and Development college and it was there that I discovered my passion for programming.",
     p2: "I started developing in Wordpress and soon discovered the world of React and React Native that since then I've been studying and learning every day.",
-    p3: 'I believe that the time has come to enter the world of programming once and for all, and that is why I ended up creating this site to serve as a platform to expose my ideas.',
+    p3: 'I believe that the time has come to enter the world of programming once and for all, and that is why I ended up creating this site to serve as a platform to expose my projects and ideas.',
 }
 
 const study = {
